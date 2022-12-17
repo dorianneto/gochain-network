07b724509a9fc03eb87d5d49fb70e60095eea0a5
Dorian <dorian@heyorca.com>
2022-12-17 19:09:54 -0330

    feat: add TCP network connection

--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"io"
 	"log"
+	"net"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/dorianneto/gochain/blockchain"
-	"github.com/dorianneto/gochain/http"
 	"github.com/joho/godotenv"
 )
+
+var blockchainServer chan []blockchain.Block
+
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+
+	io.WriteString(connection, "Enter a new BPM:")
+
+	scanner := bufio.NewScanner(connection)
+
+	go func() {
+		for scanner.Scan() {
+			bpm, err := strconv.Atoi(scanner.Text())
+
+			if err != nil {
+				log.Printf("%v not a number: %v", scanner.Text(), err)
+				continue
+			}
+
+			oldBlock := blockchain.Blockchain[len(blockchain.Blockchain)-1]
+			newBlock, err := blockchain.GenerateBlock(oldBlock, bpm)
+
+			if err != nil {
+				log.Println(err)
+			}
+
+			if blockchain.IsBlockValid(newBlock, oldBlock) {
+				newBlockchain := append(blockchain.Blockchain, newBlock)
+				blockchain.ReplaceChain(newBlockchain)
+			}
+
+			blockchainServer <- blockchain.Blockchain
+			io.WriteString(connection, "\nEnter a new BPM:")
+		}
+	}()
+}
 
 func main() {
 	err := godotenv.Load()
@@ -17,14 +57,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	go func() {
-		timestamp := time.Now()
-		genesisBlock := blockchain.Block{0, timestamp.String(), 0, "", ""}
+	blockchainServer = make(chan []blockchain.Block)
 
-		spew.Dump(genesisBlock)
+	timestamp := time.Now()
+	genesisBlock := blockchain.Block{Index: 0, Timestamp: timestamp.String(), BPM: 0, Hash: "", PrevHash: ""}
 
-		blockchain.Blockchain = append(blockchain.Blockchain, genesisBlock)
-	}()
+	spew.Dump(genesisBlock)
 
-	log.Fatal(http.Run())
+	blockchain.Blockchain = append(blockchain.Blockchain, genesisBlock)
+
+	server, err := net.Listen("tcp", ":"+os.Getenv("TCP_PORT"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer server.Close()
+
+	for {
+		connection, err := server.Accept()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConnection(connection)
+	}
 }
